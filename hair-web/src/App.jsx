4afb35e66bcb75dbe1c14e3337f93782e3793fa6
Lovegirl B <lovegirlb@Lovegirls-MacBook-Air.local>
2025-05-02 20:39:29 -0400

    Header no color

--- hair-web/src/App.jsx
+++ hair-web/src/App.jsx
@@ -1,14 +1,16 @@
-
-import Navbar from "./components/Navbar"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
-
   return (
-    <>
-      <Navbar/>
-      
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1 style={{ padding: '30px' }}>Home Page</h1>} />
+        {/* Add more routes here */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
